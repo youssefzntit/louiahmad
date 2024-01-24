c891de0dd97c33da0a2ce828b4b01ae95f01b032
youssefzntit <154595412+youssefzntit@users.noreply.github.com>
2024-01-24 14:56:48 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-SUDO = int(os.environ.get("SUDO", ""))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6361480431:AAF7g8BRquOXyBeVu8_f_kpp9gDwXDBVDSY")
+SUDO = int(os.environ.get("SUDO", "6653027663"))
 HEROKU = os.environ.get("HEROKU", "")
 APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
